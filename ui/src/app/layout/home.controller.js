4db2845391615d4fac3f4e3c603f0e95bf0597ac
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2018-01-17 17:00:23 -0500

    Home logo update

--- ui/src/app/layout/home.controller.js
+++ ui/src/app/layout/home.controller.js
@@ -18,7 +18,7 @@ import $ from 'jquery';
 
 /* eslint-disable import/no-unresolved, import/default */
 
-import logoSvg from '../../svg/hm-tempus-logo.svg';
+import logoSvg from '../../svg/Tempus_Logo_E_TagLineExtended_vectorized.svg';
 
 /* eslint-enable import/no-unresolved, import/default */
 
