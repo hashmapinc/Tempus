ac9997ce0aaf835b553db0f980a0a5cb9572cca6
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2018-02-14 17:50:54 -0500

    Tempus name update

--- ui/src/app/app.run.js
+++ ui/src/app/app.run.js
@@ -138,9 +138,9 @@ export default function AppRun($rootScope, $window, $injector, $location, $log, 
             }
             if (angular.isDefined(to.data.pageTitle)) {
                 $translate(to.data.pageTitle).then(function (translation) {
-                    $rootScope.pageTitle = 'ThingsBoard | ' + translation;
+                    $rootScope.pageTitle = 'Tempus | ' + translation;
                 }, function (translationId) {
-                    $rootScope.pageTitle = 'ThingsBoard | ' + translationId;
+                    $rootScope.pageTitle = 'Tempus | ' + translationId;
                 });
             }
         })
