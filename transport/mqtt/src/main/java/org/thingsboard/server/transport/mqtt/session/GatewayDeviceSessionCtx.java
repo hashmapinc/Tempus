c824fa641db4c4035aff52412c2bcb78145a972d
Mitesh Rathore <mitesh.rathore@hashmapinc.com>
2018-04-08 14:33:01 -0400

    message id error fix

--- transport/mqtt/src/main/java/org/thingsboard/server/transport/mqtt/session/GatewayDeviceSessionCtx.java
+++ transport/mqtt/src/main/java/org/thingsboard/server/transport/mqtt/session/GatewayDeviceSessionCtx.java
@@ -83,8 +83,8 @@ public class GatewayDeviceSessionCtx extends DeviceAwareSessionContext {
                 if (responseMsg.isSuccess()) {
                     MsgType requestMsgType = responseMsg.getRequestMsgType();
                     Integer requestId = responseMsg.getRequestId();
-                    if (requestMsgType == MsgType.POST_ATTRIBUTES_REQUEST || requestMsgType == MsgType.POST_TELEMETRY_REQUEST
-                            || requestMsgType == MsgType.POST_TELEMETRY_REQUEST_DEPTH) {
+                    if (requestId >= 0 && (requestMsgType == MsgType.POST_ATTRIBUTES_REQUEST || requestMsgType == MsgType.POST_TELEMETRY_REQUEST
+                            || requestMsgType == MsgType.POST_TELEMETRY_REQUEST_DEPTH)) {
                         return Optional.of(MqttTransportHandler.createMqttPubAckMsg(requestId));
                     }
                 }
