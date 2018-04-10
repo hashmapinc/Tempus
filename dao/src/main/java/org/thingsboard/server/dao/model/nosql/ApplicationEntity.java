3bfded332e7ded6981d3792a2a48f46122a60ce3
Jetinder <jetinder.singhrathore@gamil.com>
2018-04-10 13:53:47 -0400

    Waffle-178 => Changing description from String to json node

--- dao/src/main/java/org/thingsboard/server/dao/model/nosql/ApplicationEntity.java
+++ dao/src/main/java/org/thingsboard/server/dao/model/nosql/ApplicationEntity.java
@@ -123,7 +123,7 @@ public final class ApplicationEntity implements SearchTextEntity<Application> {
         this.name = application.getName();
         this.isValid = application.getIsValid();
         this.additionalInfo = application.getAdditionalInfo();
-        this.deviceTypes = mapper.treeToValue(application.getDeviceTypes(), DeviceTypeConfigurations.class).getDeviceTypes().stream().map(DeviceType::getName).collect(Collectors.toSet());
+        this.deviceTypes = mapper.treeToValue(application.getDeviceTypes(), DeviceTypeConfigurations.class).getConfiguration().getDeviceTypes().stream().map(DeviceType::getName).collect(Collectors.toSet());
         this.state = application.getState();
     }
 
