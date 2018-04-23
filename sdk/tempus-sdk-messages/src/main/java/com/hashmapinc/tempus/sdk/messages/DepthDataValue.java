3d3659d972b05e0af48771ac5caae621f6657799
Ed Fron <ed.fron@hashmapinc.com>
2018-04-23 16:44:26 -0600

    add license:format goal to pom

--- sdk/tempus-sdk-messages/src/main/java/com/hashmapinc/tempus/sdk/messages/DepthDataValue.java
+++ sdk/tempus-sdk-messages/src/main/java/com/hashmapinc/tempus/sdk/messages/DepthDataValue.java
@@ -1,9 +1,26 @@
+/**
+ * Copyright © 2017-2018 Hashmap, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hashmapinc.tempus.sdk.messages;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.HashMap;
 import java.util.Map;
+
+//TODO: javadoc format for each file //*  */ with min of 50%
 
 public class DepthDataValue {
 
@@ -30,7 +47,10 @@ public class DepthDataValue {
     }
 
     public void addValue(String key, Object value){
+        //TODO: add type check on supported values (boolean, string, double, long) throw un supported type exception
         values.put(key, value);
     }
+
+    //TODO:  add 4 helper methods
 
 }
