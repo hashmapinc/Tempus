eb2dad6017078f7b51da6c094f2707de6e1a0389
jetinder20 <jetinder.singhrathore@gamil.com>
2018-06-07 15:57:51 -0400

    Making Tempus a resource server

---
+++ application/src/main/java/com/hashmapinc/server/config/TempusResourceServer.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright © 2017-2018 Hashmap, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hashmapinc.server.config;
+
+import com.hashmapinc.server.exception.TempusErrorResponseHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.UserAuthenticationConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+
+@Configuration
+@EnableResourceServer
+public class TempusResourceServer extends ResourceServerConfigurerAdapter {
+
+    @Value("${security.jwt.tokenSigningKey}")
+    private String tokenSigningKey;
+
+    public static final String WEBJARS_ENTRY_POINT = "/webjars/**";
+    public static final String DEVICE_API_ENTRY_POINT = "/api/v1/**";
+    public static final String FORM_BASED_LOGIN_ENTRY_POINT = "/api/auth/login";
+    public static final String PUBLIC_LOGIN_ENTRY_POINT = "/api/auth/login/public";
+    public static final String TOKEN_REFRESH_ENTRY_POINT = "/api/auth/token";
+    protected static final String[] NON_TOKEN_BASED_AUTH_ENTRY_POINTS = new String[] {"/index.html", "/static/**", "/api/noauth/**", "/api/theming/**", "/webjars/**"};
+    public static final String TOKEN_BASED_AUTH_ENTRY_POINT = "/api/**";
+    public static final String WS_TOKEN_BASED_AUTH_ENTRY_POINT = "/api/ws/**";
+
+    public static final String EXTERNAL_API_WRITE_ENDPOINTS = "/api/external/write/**";
+
+    private static final String TEMPUS_RESOURCE_ID = "tempus-resource-server-rest-api";
+
+
+    @Autowired
+    private TempusErrorResponseHandler restAccessDeniedHandler;
+
+    @Autowired
+    private UserAuthenticationConverter userDetailsConverter;
+
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer config) {
+        config.resourceId(TEMPUS_RESOURCE_ID);
+        config.tokenServices(tokenServices());
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
+    }
+
+
+    /*Todo Is this needed or just one implemented is enough?
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenService(){
+        DefaultTokenServices service = new DefaultTokenServices();
+        service.setTokenStore(tokenStore());
+        service.setSupportRefreshToken(true);
+        service.setAuthenticationManager(authenticationManager);
+        service.setRefreshTokenValiditySeconds(settings.getRefreshTokenExpTime());
+        service.setAccessTokenValiditySeconds(settings.getTokenExpirationTime());
+        return service;
+    }*/
+
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
+        accessTokenConverter.setUserTokenConverter(userDetailsConverter);
+        converter.setAccessTokenConverter(accessTokenConverter);
+        converter.setSigningKey(tokenSigningKey);
+        return converter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.headers().cacheControl().and().frameOptions().disable()
+                .and()
+                .cors()
+                .and()
+                .csrf().disable()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(WEBJARS_ENTRY_POINT).permitAll() // Webjars
+                .antMatchers(DEVICE_API_ENTRY_POINT).permitAll() // Device HTTP Transport API
+                .antMatchers(FORM_BASED_LOGIN_ENTRY_POINT).permitAll() // Login end-point
+                .antMatchers(PUBLIC_LOGIN_ENTRY_POINT).permitAll() // Public login end-point
+                .antMatchers(TOKEN_REFRESH_ENTRY_POINT).permitAll() // Token refresh end-point
+                .antMatchers(NON_TOKEN_BASED_AUTH_ENTRY_POINTS).permitAll() // static resources, user activation and password reset end-points
+                .and()
+                .authorizeRequests()
+                .antMatchers(WS_TOKEN_BASED_AUTH_ENTRY_POINT).authenticated() // Protected WebSocket API End-points
+                .antMatchers(TOKEN_BASED_AUTH_ENTRY_POINT).authenticated() // Protected API End-points
+                .and()
+                .exceptionHandling().accessDeniedHandler(restAccessDeniedHandler);
+    }
+
+}
