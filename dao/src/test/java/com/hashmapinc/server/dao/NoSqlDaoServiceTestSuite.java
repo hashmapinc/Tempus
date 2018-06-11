919b16546d2c62986e0dff0d2f0c252ba43683c7
Shobhit2884 <shobhit.sharma@hashmapinc.com>
2018-06-11 16:58:21 +0530

    Rebase with dev

--- dao/src/test/java/com/hashmapinc/server/dao/NoSqlDaoServiceTestSuite.java
+++ dao/src/test/java/com/hashmapinc/server/dao/NoSqlDaoServiceTestSuite.java
@@ -43,8 +43,11 @@ public class NoSqlDaoServiceTestSuite {
         dataSets.add(new ClassPathCQLDataSet("cassandra/system-data.cql", false, false));
         dataSets.add(new ClassPathCQLDataSet("cassandra/system-test.cql", false, false));
         dataSets.addAll(Arrays.asList(
-                new ClassPathCQLDataSet("cassandra/upgrade/1.cql", false, false)
+                new ClassPathCQLDataSet("cassandra/upgrade/1.cql", false, false),
+                new ClassPathCQLDataSet("cassandra/upgrade/3.cql", false, false)
         ));
+
+
 
         return dataSets;
     }
