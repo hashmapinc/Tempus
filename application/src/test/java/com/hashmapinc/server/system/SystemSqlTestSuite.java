0e69b97946698f90c146471529fcdfe8f71aa40e
Shobhit2884 <shobhit.sharma@hashmapinc.com>
2018-06-11 19:08:22 +0530

    Commit code for shifting upgrade script to respective folder

--- application/src/test/java/com/hashmapinc/server/system/SystemSqlTestSuite.java
+++ application/src/test/java/com/hashmapinc/server/system/SystemSqlTestSuite.java
@@ -34,7 +34,7 @@ public class SystemSqlTestSuite {
             Arrays.asList("sql/hsql/schema.sql", "sql/system-data.sql"),
             "sql/drop-all-tables.sql",
             "sql-test.properties",
-            Arrays.asList("sql/hsql/upgrade/1.sql", "sql/hsql/upgrade/2.sql", "sql/upgrade/4.sql")
+            Arrays.asList("sql/hsql/upgrade/1.sql", "sql/hsql/upgrade/2.sql", "sql/hsql/upgrade/4.sql")
 
 
     );
