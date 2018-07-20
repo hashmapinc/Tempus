cf97a38a04493a78fb2acf87fb852445f1586423
Jay <jayesh.kapadnis@gmail.com>
2018-07-20 12:13:23 +0530

    Fixing tests for  branch

--- dao/src/test/java/com/hashmapinc/server/dao/SqlDaoServiceTestSuite.java
+++ dao/src/test/java/com/hashmapinc/server/dao/SqlDaoServiceTestSuite.java
@@ -33,5 +33,5 @@ public class SqlDaoServiceTestSuite {
             Arrays.asList("sql/hsql/schema.sql", "sql/system-data.sql"),
             "sql/drop-all-tables.sql",
             "sql-test.properties",
-            Arrays.asList("sql/hsql/upgrade/1.sql" , "sql/hsql/upgrade/2.sql"));
+            Arrays.asList("sql/hsql/upgrade/1.sql" , "sql/hsql/upgrade/2.sql", "sql/hsql/upgrade/3.sql"));
 }
