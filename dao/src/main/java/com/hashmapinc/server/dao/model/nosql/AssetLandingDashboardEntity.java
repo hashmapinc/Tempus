59292b829cbb240a98644e09cde6e7a1ba009988
himanshu <hmdcisin@gmail.com>
2018-08-08 17:49:58 +0530

    Tempus-652 Added asset_landing_dashboard table in cassandra db also.

--- dao/src/main/java/com/hashmapinc/server/dao/model/nosql/AssetLandingDashboardEntity.java
+++ dao/src/main/java/com/hashmapinc/server/dao/model/nosql/AssetLandingDashboardEntity.java
@@ -14,10 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.hashmapinc.server.dao.model.sql;
+package com.hashmapinc.server.dao.model.nosql;
 
+import com.datastax.driver.mapping.annotations.Column;
+import com.datastax.driver.mapping.annotations.PartitionKey;
+import com.datastax.driver.mapping.annotations.Table;
 import com.hashmapinc.server.common.data.AssetLandingDashboard;
-import com.hashmapinc.server.common.data.UUIDConverter;
 import com.hashmapinc.server.common.data.id.DashboardId;
 import com.hashmapinc.server.common.data.id.DataModelId;
 import com.hashmapinc.server.common.data.id.DataModelObjectId;
@@ -26,46 +28,42 @@ import com.hashmapinc.server.dao.model.ToData;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.UUID;
 
 @Data
 @EqualsAndHashCode
-@Entity
 @Table(name = ModelConstants.ASSET_LANDING_COLUMN_FAMILY_NAME)
-public class AssetLandingDashboardEntity implements ToData<AssetLandingDashboard> {
+public class AssetLandingDashboardEntity implements ToData<AssetLandingDashboard>{
 
-    @Id
+    @PartitionKey(value = 0)
     @Column(name = ModelConstants.ASSET_LANDING_DASHBOARD_ID)
-    private String dashboardId;
+    private UUID dashboardId;
 
     @Column(name = ModelConstants.ASSET_LANDING_DATA_MODEL_ID)
-    private String dataModelId;
+    private UUID dataModelId;
 
     @Column(name = ModelConstants.ASSET_LANDING_DATAMODEL_OBJECT_ID)
-    private String dataModelObjectId;
+    private UUID dataModelObjectId;
 
     public AssetLandingDashboardEntity(AssetLandingDashboard assetLandingDashboard){
         if (assetLandingDashboard.getDashboardId() != null) {
-            this.dashboardId = UUIDConverter.fromTimeUUID(assetLandingDashboard.getDashboardId().getId());
+            this.dashboardId = assetLandingDashboard.getDashboardId().getId();
         }
         if (assetLandingDashboard.getDataModelId() != null) {
-            this.dataModelId = UUIDConverter.fromTimeUUID(assetLandingDashboard.getDataModelId().getId());
+            this.dataModelId = assetLandingDashboard.getDataModelId().getId();
         }
         if (assetLandingDashboard.getDataModelObjectId() != null) {
-            this.dataModelObjectId = UUIDConverter.fromTimeUUID(assetLandingDashboard.getDataModelObjectId().getId());
+            this.dataModelObjectId = assetLandingDashboard.getDataModelObjectId().getId();
         }
     }
 
     @Override
     public AssetLandingDashboard toData() {
-        AssetLandingDashboard landingDashboard = new AssetLandingDashboard(new DashboardId(UUIDConverter.fromString(this.getDashboardId())));
+        AssetLandingDashboard landingDashboard = new AssetLandingDashboard(new DashboardId(this.getDashboardId()));
         if (dataModelId != null)
-            landingDashboard.setDataModelId(new DataModelId(UUIDConverter.fromString(this.getDataModelId())));
+            landingDashboard.setDataModelId(new DataModelId(this.getDataModelId()));
         if (dataModelObjectId != null)
-            landingDashboard.setDataModelObjectId(new DataModelObjectId(UUIDConverter.fromString(this.getDataModelObjectId())));
+            landingDashboard.setDataModelObjectId(new DataModelObjectId(this.getDataModelObjectId()));
         return landingDashboard;
     }
 }
