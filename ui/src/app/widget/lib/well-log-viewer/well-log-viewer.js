f06014983ffca9890e73c5b3d8075076fdcc0819
Ganesh Hegde <ganesh.hegde@hashmapinc.com>
2018-08-14 09:00:42 -0400

    adding settings schema

--- ui/src/app/widget/lib/well-log-viewer/well-log-viewer.js
+++ ui/src/app/widget/lib/well-log-viewer/well-log-viewer.js
@@ -20,15 +20,111 @@ import loadLogViewer from './logViewer';
 //import * as welllogviwer from 'WellLogViewer'
 
 /* eslint-disable angular/angularelement */
+/* eslint-disable */
 export default class WellLogViewer {
-     constructor() {
+     constructor(ctx) {
+     	this.ctx = ctx;
+          //  var settings = ctx.settings;
             this.init();
      }
      init() {
-		loadLogViewer();
+		//loadLogViewer();
         //alert(test);
         //alert("test.test");
      }
+
+     test(){
+     	var datasources = this.ctx.datasources;
+        console.log(settings);
+        var dataKeys = [];
+        datasources.forEach(function(dkeys){
+        	dkeys.dataKeys.forEach(function(datakey){
+
+        		dataKeys.push({"value": datakey.label, "label": datakey.label});
+        	})
+        })
+     	return dataKeys;
+     }
+
+     update() {
+     	loadLogViewer(this.ctx);
+     	console.log(this.ctx);
+     }
+
+     static settingsSchema(ctx){
+		return {
+			"schema":{
+			    "type": "object",
+			    "properties": {
+			      "Track": {
+			        "type": "array",
+			        "items": {
+			              "title": "Settings",
+			              "type": "object",
+			              "properties":{
+			                  "component":{
+			                      "title":"Component",
+			                      "type": "array",
+			                      "items": {
+			                          "title": "Settings",
+			                          "type": "object",
+			                          "properties":{
+			                              "ComponentType":{
+			                                  "title":"Type",
+			                                  "type": "string",
+			                                  "enum": ["yy","tt"]
+			                              },
+			                              "lineWidth":{
+			                                  "title":"Line Width",
+			                                  "type": "string"
+			                              }
+			                            }       
+			                      }
+			                  },
+			                  "test":{
+			                      "title": "tttt",
+			                      "type": "string"
+			                  },
+			                  "test1":{
+			                      "title": "yyyy",
+			                      "type": "string"
+			                  },
+			                  "test2":{
+			                  	"title": "Datasource",
+								"type": "string"
+								}
+			                }       
+			          
+			        }
+			      }
+			    }
+			  },
+		"form":[{
+	      "key": "Track",
+	      "items": [
+	           {
+	              "key": "Track[].component",
+	              "items": [
+	                "Track[].component[].ComponentType",
+	                {
+	                  "key": "Track[].component[].lineWidth",
+	                  "condition": "model.Track[form.key[1]].component[form.arrayIndex].ComponentType === 'one'"
+	                }
+	              ]
+	            },
+	           "Track[].test1",
+	           {
+                    "key": "Track[].test2",
+                    "type": "rc-select",
+                    "multiple": false,
+                    "items": ["one", "two"]
+                }
+	      ]
+	}]
+
+
+		};
+	}
 }
 
 /* eslint-enable angular/angularelement */
