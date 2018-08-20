947312bee8bbfc2188483546ce86b3a2bda8a3e0
shubhamkumargupta <shubhamkumargupta09@gmail.com>
2018-08-20 15:25:02 +0530

    Adding customer group controller intergration tests

---
+++ application/src/it/java/com/hashmapinc/server/controller/nosql/CustomerGroupControllerNoSqlTest.java
@@ -0,0 +1,8 @@
+package com.hashmapinc.server.controller.nosql;
+
+import com.hashmapinc.server.controller.BaseCustomerGroupControllerTest;
+import com.hashmapinc.server.dao.service.DaoNoSqlTest;
+
+@DaoNoSqlTest
+public class CustomerGroupControllerNoSqlTest extends BaseCustomerGroupControllerTest {
+}
