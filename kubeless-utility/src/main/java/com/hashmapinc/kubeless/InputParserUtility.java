52e13899b17d2be7b27c73fe67fe446405281c61
himanshu <hmdcisin@gmail.com>
2018-09-25 15:42:01 +0530

    Tempus-744 Added validation for user defined key/tag.

--- kubeless-utility/src/main/java/com/hashmapinc/kubeless/InputParserUtility.java
+++ kubeless-utility/src/main/java/com/hashmapinc/kubeless/InputParserUtility.java
@@ -4,16 +4,27 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputParserUtility {
 
-    private List<String> keysToValidate = Arrays.asList("id", "ts", "ds");;
+    private List<String> keysToValidate = Arrays.asList("id", "ts", "ds");
+
     public InputParserUtility() {
     }
 
+
     public boolean validateJson(String json) throws IOException {
+        return validateJson(json, Collections.EMPTY_LIST);
+    }
+
+    public boolean validateJson(String json, List<String> additionalKeys) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         final JsonNode jsonNode = objectMapper.readTree(json);
         List<String> fieldNames = new ArrayList<>();
@@ -21,20 +32,22 @@ public class InputParserUtility {
 
         AtomicBoolean validationStatus = new AtomicBoolean(true);
 
-        keysToValidate.forEach(key -> {
-            if (key.contentEquals("ts") || key.contentEquals("ds")) {
-                boolean timestampPresent = fieldNames.contains("ts");
-                boolean depthstampPresent = fieldNames.contains("ds");
-                if (timestampPresent && depthstampPresent) {
-                    validationStatus.set(false);
-                }
-                if (!timestampPresent && !depthstampPresent) {
-                    validationStatus.set(false);
-                }
-            } else if (!fieldNames.contains(key)){
-                validationStatus.set(false);
-            }
-        });
+        Stream.concat(keysToValidate.stream(), additionalKeys.stream())
+                .collect(Collectors.toList())
+                .forEach(key -> {
+                    if (key.contentEquals("ts") || key.contentEquals("ds")) {
+                        boolean timestampPresent = fieldNames.contains("ts");
+                        boolean depthstampPresent = fieldNames.contains("ds");
+                        if (timestampPresent && depthstampPresent) {
+                            validationStatus.set(false);
+                        }
+                        if (!timestampPresent && !depthstampPresent) {
+                            validationStatus.set(false);
+                        }
+                    } else if (!fieldNames.contains(key)) {
+                        validationStatus.set(false);
+                    }
+                });
         return validationStatus.get();
     }
 }
--- kubeless-utility/src/test/java/InputParserUtilityTest.java
+++ kubeless-utility/src/test/java/InputParserUtilityTest.java
@@ -3,6 +3,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class InputParserUtilityTest {
     @Test
@@ -34,7 +37,29 @@ public class InputParserUtilityTest {
         InputParserUtility inputParserUtility = new InputParserUtility();
         String jsonStr = "{\"id\":\"1\", \"ts\":\"45676\", \"ds\":\"3000\"}";
         try {
-            Assert.assertEquals(inputParserUtility.validateJson(jsonStr),false);
+            Assert.assertEquals(inputParserUtility.validateJson(jsonStr, Collections.EMPTY_LIST),false);
+        } catch (IOException e) {
+
+        }
+    }
+
+    @Test
+    public void validateCorrectJsonWithAddedKey() {
+        InputParserUtility inputParserUtility = new InputParserUtility();
+        String jsonStr = "{\"id\":\"1\", \"ts\":\"45676\", \"key\":\"val\"}";
+        try {
+            Assert.assertEquals(inputParserUtility.validateJson(jsonStr, Collections.singletonList("key")),true);
+        } catch (IOException e) {
+
+        }
+    }
+
+    @Test
+    public void validateCorrectJsonWithoutAddedKey() {
+        InputParserUtility inputParserUtility = new InputParserUtility();
+        String jsonStr = "{\"id\":\"1\", \"ts\":\"45676\"}";
+        try {
+            Assert.assertEquals(inputParserUtility.validateJson(jsonStr, Collections.singletonList("key")),false);
         } catch (IOException e) {
 
         }
