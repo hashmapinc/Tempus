6009a8f795dcb1cdcb1c3c1c06e246dcc0701943
NandiniMGawade <nandini.gawade@hashmapinc.com>
2018-11-27 14:59:31 +0530

    Added logo in tenant schema.

--- dao/src/main/resources/sql/hsql/schema.sql
+++ dao/src/main/resources/sql/hsql/schema.sql
@@ -208,7 +208,8 @@ CREATE TABLE IF NOT EXISTS tenant (
     search_text varchar(255),
     state varchar(255),
     title varchar(255),
-    zip varchar(255)
+    zip varchar(255),
+    logo_file VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS ts_kv (
