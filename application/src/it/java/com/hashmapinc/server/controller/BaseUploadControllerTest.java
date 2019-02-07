a3d3b96f40faf80e009843be9f10cc8963803fb1
Himanshu Mishra <hmdcisin@gmail.com>
2019-02-07 13:08:28 +0530

    Tempus-991 Backend changes for s3 file rename.

--- application/src/it/java/com/hashmapinc/server/controller/BaseUploadControllerTest.java
+++ application/src/it/java/com/hashmapinc/server/controller/BaseUploadControllerTest.java
@@ -35,7 +35,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
