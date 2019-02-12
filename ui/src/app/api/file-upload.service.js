e650d5c086f302e307b8dce04b235a093e285122
rachanagupta01 <rachana.gupta@hashmapinc.com>
2019-02-12 17:33:40 +0530

    Tempus-991 UI changes for upload, download, rename and delete.

--- ui/src/app/api/file-upload.service.js
+++ ui/src/app/api/file-upload.service.js
@@ -17,197 +17,256 @@
 
 
 export default angular.module('tempus.api.fileUpload', [])
-    .factory('fileUploadService', FileUploadService)
-    .name;
+	.factory('fileUploadService', FileUploadService)
+	.name;
 
 /*@ngInject*/
-function FileUploadService(toast,$http, $q, $translate, $window, $document, $injector) {
+function FileUploadService(toast, $http, $q, $translate, $window, $document, $injector) {
 
-    var service = {
+	var service = {
 
-        getAllFile: getAllFile,
-        uploadFile: uploadFile,
-        deleteFile: deleteFile,
-        exportFile: exportFile,
-        renameFile: renameFile,
-        fileValidation: fileValidation,
-        extensionValidation: extensionValidation,
-        searchFile: searchFile
-        extensionValidation: extensionValidation
+		getAllFile: getAllFile,
+		uploadFile: uploadFile,
+		deleteFile: deleteFile,
+		exportFile: exportFile,
+		renameFile: renameFile,
+		fileValidation: fileValidation,
+		extensionValidation: extensionValidation,
+		searchFile: searchFile,
+		newNameValidation: newNameValidation
 
-    }
+	}
 
-    return service;
+	return service;
 
-    function uploadFile(file) {
-        var deferred = $q.defer();
-        var url = '/api/file';
-        var fd = new FormData();
-        fd.append("file", file);
-        $http.post(url, fd, {
-                    transformRequest: angular.identity,
-                    headers: {'Content-Type': undefined}
-                }).then(function success(response) {
-                      deferred.resolve(response.data);
-                  }, function fail(response) {
-                      deferred.reject(response.data);
-                  });
-        return deferred.promise;
-    }
+	function uploadFile(file) {
+		var deferred = $q.defer();
+		var url = '/api/file';
+		var fd = new FormData();
+		fd.append("file", file);
+		$http.post(url, fd, {
+			transformRequest: angular.identity,
+			headers: {
+				'Content-Type': undefined
+			}
+		}).then(function success(response) {
+			deferred.resolve(response.data);
+		}, function fail(response) {
+			deferred.reject(response.data);
+		});
+		return deferred.promise;
+	}
 
-    function exportFile(fileName) {
-            var name = fileName;
-            getFile(fileName).then(
-                function success(file) {
-                    exportToPc(prepareExport(file), name);
-                },
-                function fail(rejection) {
-                    var message = rejection;
-                    if (!message) {
-                        message = $translate.instant('error.unknown-error');
-                    }
+	function exportFile(fileName, extension) {
+		if (extension != getTypes().fileUpload.na && extension != "") {
+			var name = fileName + "." + extension;
+		} else {
+			name = fileName;
+		}
 
-                }
-            );
-        }
+		getFile(name).then(
+			function success(file) {
+				exportToPc(prepareExport(file), name);
+			},
+			function fail(rejection) {
+				var message = rejection;
+				if (!message) {
+					message = $translate.instant('error.unknown-error');
+				}
 
-    function prepareExport(data) {
-            var exportedData = angular.copy(data);
-            if (angular.isDefined(exportedData.id)) {
-                delete exportedData.id;
-            }
-            if (angular.isDefined(exportedData.createdTime)) {
-                delete exportedData.createdTime;
-            }
-            if (angular.isDefined(exportedData.tenantId)) {
-                delete exportedData.tenantId;
-            }
-            if (angular.isDefined(exportedData.customerId)) {
-                delete exportedData.customerId;
-            }
-            return exportedData;
-    }
-    function exportToPc(data, filename) {
-        if (!data) {
-            return;
-        }
-        else if (angular.isObject(data)) {
-            data = angular.toJson(data, 2);
-        }
+			}
+		);
+	}
 
-        if (!filename) {
-            filename = 'downloaded file';
-        }
+	function prepareExport(data) {
+		var exportedData = angular.copy(data);
+		if (angular.isDefined(exportedData.id)) {
+			delete exportedData.id;
+		}
+		if (angular.isDefined(exportedData.createdTime)) {
+			delete exportedData.createdTime;
+		}
+		if (angular.isDefined(exportedData.tenantId)) {
+			delete exportedData.tenantId;
+		}
+		if (angular.isDefined(exportedData.customerId)) {
+			delete exportedData.customerId;
+		}
+		return exportedData;
+	}
 
+	function exportToPc(data, filename) {
+		if (!data) {
+			return;
+		} else if (angular.isObject(data)) {
+			data = angular.toJson(data, 2);
+		}
 
-        var blob = new Blob([data]);
-
-        if ($window.navigator && $window.navigator.msSaveOrOpenBlob) {
-            $window.navigator.msSaveOrOpenBlob(blob, filename);
-        }
-        else{
-            var e = $document[0].createEvent('MouseEvents'),
-                a = $document[0].createElement('a');
-
-            a.download = filename;
-            a.href = $window.URL.createObjectURL(blob);
-            a.dataset.downloadurl = [ a.download, a.href].join(':');
-            e.initEvent('click', true, false, $window,
-                0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            a.dispatchEvent(e);
-        }
-    }
-    function getFile(fileName) {
-         var deferred = $q.defer();
-         var url = '/api/file/' + fileName;
-         $http.get(url).then(function success(response) {
-            deferred.resolve(response.data);
-         }, function fail(response) {
-            deferred.reject(response.data);
-         });
-         return deferred.promise;
-    }
+		if (!filename) {
+			filename = 'downloaded file';
+		}
 
 
-    function getAllFile() {
-        var deferred = $q.defer();
-        var url = '/api/file';
-        $http.get(url).then(function success(response) {
-            deferred.resolve(response.data);
-        }, function fail() {
-            deferred.reject();
-        });
-        return deferred.promise;
-    }
-    function deleteFile(fileName) {
-        var deferred = $q.defer();
-        var url = '/api/file/' + fileName;
-        $http.delete(url).then(function success() {
-            deferred.resolve();
-        }, function fail() {
-            deferred.reject();
-        });
-        return deferred.promise;
-    }
+		var blob = new Blob([data]);
+
+		if ($window.navigator && $window.navigator.msSaveOrOpenBlob) {
+			$window.navigator.msSaveOrOpenBlob(blob, filename);
+		} else {
+			var e = $document[0].createEvent('MouseEvents'),
+				a = $document[0].createElement('a');
+
+			a.download = filename;
+			a.href = $window.URL.createObjectURL(blob);
+			a.dataset.downloadurl = [a.download, a.href].join(':');
+			e.initEvent('click', true, false, $window,
+				0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			a.dispatchEvent(e);
+		}
+	}
+
+	function getFile(fileName) {
+		var deferred = $q.defer();
+		var url = '/api/file/' + fileName;
+		$http.get(url).then(function success(response) {
+			deferred.resolve(response.data);
+		}, function fail(response) {
+			deferred.reject(response.data);
+		});
+		return deferred.promise;
+	}
 
 
-    function renameFile(oldFileName, newFileName) {
-            var deferred = $q.defer();
-            var url = '/api/file/' + oldFileName;
-            $http.put(url, newFileName, {
-                        transformRequest: angular.identity,
-                        headers: {'Content-Type': undefined}
-                    }).then(function success(response) {
-                          deferred.resolve(response.data);
-                      }, function fail(response) {
-                          deferred.reject(response.data);
-                      });
-            return deferred.promise;
-    }
+	function getAllFile() {
+		var deferred = $q.defer();
+		var url = '/api/file';
+		$http.get(url).then(function success(response) {
+			deferred.resolve(response.data);
+		}, function fail() {
+			deferred.reject();
+		});
+		return deferred.promise;
+	}
 
-    function searchFile(fileName){
-            var deferred = $q.defer();
-            var url = '/api/file?fileName=' + fileName;
-            $http.get(url, {
-                    transformRequest: angular.identity,
-                    headers: {'Content-Type': undefined}
-                }).then(function success(response) {
-                deferred.resolve(response.data);
-            }, function fail(response) {
-                deferred.reject(response.data);
-            });
-            return deferred.promise;
-    }
+	function deleteFile(fileName, extension) {
 
-    function fileValidation(fileToBeUploaded){
+		if (extension != getTypes().fileUpload.na && extension != "") {
+			var url = '/api/file/' + fileName; + "." + extension;
+		} else {
+			url = '/api/file/' + fileName;
+		}
 
-         if(extensionValidation(fileToBeUploaded[0].name)=== true){
-             if(fileToBeUploaded[0].size <= getTypes().fileUpload.maxSize){
-                 return true;
-             }
-             else{
-                 toast.showError($translate.instant('file-upload.fileSizeError'));
-             }
-         }
-         else{
-             toast.showError($translate.instant('file-upload.fileExtensionError'));
-         }
-    }
+		var deferred = $q.defer();
+		$http.delete(url).then(function success() {
+			deferred.resolve();
+			toast.showSuccess($translate.instant('file-upload.deleteSuccess'));
+		}, function fail() {
+			deferred.reject();
+		});
+		return deferred.promise;
+	}
 
-    function getTypes() {
-        return $injector.get("types");
-    }
 
-    function extensionValidation(fileName){
-        var ext = fileName.split(".");
-        var listOfExtension = getTypes().fileUpload.listOfExtension;
-        var extension = ext[ext.length-1];
-        if(listOfExtension.indexOf(extension) > -1){
-           return false;
-        }
-        else{
-           return true;
-          }
+	function renameFile(oldFileName, newFileName, extension) {
+		var deferred = $q.defer();
+		if (extension != getTypes().fileUpload.na && extension != "") {
+			var url = '/api/file/' + oldFileName + "." + extension;
+			newFileName = newFileName + "." + extension;
+		} else {
+			url = '/api/file/' + oldFileName;
+		}
+		$http.put(url, newFileName, {
+			transformRequest: angular.identity,
+			headers: {
+				'Content-Type': undefined
+			}
+		}).then(function success(response) {
+			deferred.resolve(response.data);
+		}, function fail(response) {
+			deferred.reject(response.data);
+		});
+		return deferred.promise;
+	}
 
-    }
+	function searchFile(fileName, extension) {
+		var deferred = $q.defer();
+
+		if (extension != getTypes().fileUpload.na && extension != "") {
+			var url = '/api/file?fileName=' + fileName + "." + extension;
+		} else {
+			url = '/api/file?fileName=' + fileName;
+		}
+		$http.get(url, {
+			transformRequest: angular.identity,
+			headers: {
+				'Content-Type': undefined
+			}
+		}).then(function success(response) {
+			deferred.resolve(response.data);
+		}, function fail(response) {
+			deferred.reject(response.data);
+		});
+		return deferred.promise;
+	}
+
+	function fileValidation(fileToBeUploaded) {
+
+		if (extensionValidation(fileToBeUploaded[0].name) === true) {
+			if (fileToBeUploaded[0].size <= getTypes().fileUpload.maxSize) {
+				return true;
+			} else {
+				toast.showError($translate.instant('file-upload.fileSizeError'));
+			}
+		} else {
+			toast.showError($translate.instant('file-upload.fileExtensionError'));
+		}
+	}
+
+	function getTypes() {
+		return $injector.get("types");
+	}
+
+
+	function newNameValidation(fileName) {
+		var deferred = $q.defer();
+		searchFile(fileName).then(
+			function success(searchItems) {
+				var existFile = 0;
+				if (searchItems.length == 0) {
+
+					existFile = 0;
+				} else {
+
+					for (var i = 0; i < searchItems.length; i++) {
+						if (searchItems[i].fileName == fileName) {
+							existFile++;
+						}
+					}
+					if (existFile == 0) {
+						return true;
+					} else {
+						toast.showError($translate.instant('file-upload.fileNameError'));
+						return false;
+					}
+				}
+			},
+			function fail() {
+				deferred.reject();
+			}
+		);
+		return deferred.promise;
+	}
+
+
+	function extensionValidation(fileName) {
+		var ext = fileName.split(".");
+		var listOfExtension = getTypes().fileUpload.listOfExtension;
+		var extension = ext[ext.length - 1];
+		if (listOfExtension.indexOf(extension) > -1) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
 }
